cbbfed0e81ff5c3691876c79f548783f1f151c5b
Karl Kaspar Haavel <karlkasparhaavel@gmail.com>
2021-04-15 12:49:30 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='nlp_colab',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='karl',
+    author_email='',
+    description='For nlp project in colab'
+)

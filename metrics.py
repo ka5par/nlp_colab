a51e0829de5c2b43e71768cc366e1f698ceb9b12
Karl Kaspar Haavel <karlkasparhaavel@gmail.com>
2021-04-15 15:53:09 +0300

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -81,6 +81,7 @@ def calc_all(gold_filename='golds.json', filepath=filepath):
                 models.append(file)
 
     gold = read_json(gold_filename)
+    print("models", models)
     models.remove(gold_filename)
 
     outputs = []
